4b161e96b032edfc8f55cf268b5010802314e967
Bohdan Klimenkov <59416603+b04dan@users.noreply.github.com>
2026-09-03 18:43:32 +0200

    feat(backend): generate project

---
+++ backend/src/main/java/de/ichbinhier/volunteerformservice/VolunteerFormServiceApplication.java
@@ -0,0 +1,14 @@
+package de.ichbinhier.volunteerformservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class VolunteerFormServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerFormServiceApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/ichbinhier/volunteerformservice/VolunteerFormServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package de.ichbinhier.volunteerformservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class VolunteerFormServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
